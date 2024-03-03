69940a7768c25d64def12d288db18fd50c33caf0
Abdul-Moiz12 <154911874+Abdul-Moiz12@users.noreply.github.com>
2024-03-03 13:55:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// calculator 
+let no1 = +prompt("Enter the First Number: ");
+let no2 = +prompt("Enter the Second Number: ");
+
+let sum = no1 + no2;
+document.write("Sum of numbers are: ", sum, "<br/>");
+
+let sub = no1 - no2;
+document.write("Subtraction of numbers are: ", sub, "<br/>");
+
+let mul = no1 * no2;
+document.write("Multiplication of numbers are: ", mul, "<br/>");
+
+
+
+let div = no1 / no2;
+document.write("Divide of numbers are: ", div, "<br/>");
+
+
+let Reminder = no1 % no2;
+document.write("Reminder of numbers are: ", Reminder, "<br/>");
+
+
+
+
